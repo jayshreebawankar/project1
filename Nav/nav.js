f39d6c791f20989e71bdb28e967e25349cf4a46f
jayshreebawankar <98347627+jayshreebawankar@users.noreply.github.com>
2022-04-04 19:51:38 +0530

    added 3 input

--- Nav/nav.js
+++ Nav/nav.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+    </div>
   )
 }
 
